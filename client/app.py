b9e650084a4e0f3b1382460d4b25584b3fcbcf7b
chriss8g <chriss.8.g@citmatel.inf.cu>
2024-11-26 00:42:19 -0500

    fix separate server and client

--- client/app.py
+++ client/app.py
@@ -1,17 +1,12 @@
 from flask import Flask, send_from_directory
-from controllers.scrape_controller import scrape_bp
-from controllers.db_controller import db_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-app.register_blueprint(scrape_bp)
-app.register_blueprint(db_bp)
 
 @app.route('/')
 def index():
     return send_from_directory('templates', 'index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3000)
