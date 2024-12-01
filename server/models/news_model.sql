78ad485f9429d3b5544674a425ceea9dc8b6da90
Amanda Cordero <amandacordero.1709@gmail.com>
2024-12-01 12:34:06 -0500

    fix .sql

--- server/models/news_model.sql
+++ server/models/news_model.sql
@@ -1,9 +1,19 @@
-CREATE TABLE news (
+CREATE TABLE URL (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    seccion VARCHAR(50),
-    titulo VARCHAR(255),
     enlace TEXT,
-    resumen TEXT,
-    comentarios VARCHAR(50),
-    imagen TEXT
+    html TEXT
+);
+
+CREATE TABLE Links (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    siteid INT,  
+    enlace TEXT,
+    FOREIGN KEY (siteid) REFERENCES URL(id)  
+);
+
+CREATE TABLE Files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    siteid INT,  
+    archivo TEXT,
+    FOREIGN KEY (siteid) REFERENCES URL(id)  
 );
